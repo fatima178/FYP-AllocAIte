3acfc6ad4958ef22fcffb674ab6e5b467ddf0512
fatima178 <c22305656@mytudublin.ie>
2025-11-02 18:11:55 +0000

    Created initial database

---
+++ backend/db.py
@@ -0,0 +1,93 @@
+import psycopg2
+
+def get_connection():
+    conn = psycopg2.connect(
+        dbname="allocaite",
+        user="fatima",             
+        password= " ",   
+        host="localhost",
+        port="5433"
+    )
+    return conn
+
+
+def init_db():
+    conn = get_connection()
+    cur = conn.cursor()
+
+    # USERS TABLE
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS Users (
+            user_id SERIAL PRIMARY KEY,
+            name VARCHAR(100),
+            email VARCHAR(100) UNIQUE,
+            password_hash VARCHAR(255),
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        );
+    """)
+
+    # UPLOADS TABLE
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS Uploads (
+            upload_id SERIAL PRIMARY KEY,
+            user_id INT REFERENCES Users(user_id),
+            file_name VARCHAR(255),
+            upload_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+            is_active BOOLEAN
+        );
+    """)
+
+    # EMPLOYEES TABLE
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS Employees (
+            employee_id SERIAL PRIMARY KEY,
+            upload_id INT REFERENCES Uploads(upload_id),
+            name VARCHAR(100),
+            role VARCHAR(100),
+            experience_years FLOAT,
+            skills JSON,
+            availability_score FLOAT,
+            department VARCHAR(100)
+        );
+    """)
+
+    # ASSIGNMENTS TABLE
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS Assignments (
+            assignment_id SERIAL PRIMARY KEY,
+            upload_id INT REFERENCES Uploads(upload_id),
+            title VARCHAR(150),
+            description TEXT,
+            required_skills JSON,
+            start_date DATE,
+            end_date DATE,
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        );
+    """)
+
+    # RECOMMENDATIONS TABLE
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS Recommendations (
+            rec_id SERIAL PRIMARY KEY,
+            assignment_id INT REFERENCES Assignments(assignment_id),
+            employee_id INT REFERENCES Employees(employee_id),
+            match_score FLOAT,
+            reason TEXT,
+            generated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        );
+    """)
+
+    # CHATLOGS TABLE
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS ChatLogs (
+            chat_id SERIAL PRIMARY KEY,
+            user_id INT REFERENCES Users(user_id),
+            query_text TEXT,
+            response_text TEXT,
+            timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        );
+    """)
+
+    conn.commit()
+    cur.close()
+    conn.close()
--- backend/main.py
+++ backend/main.py
@@ -1,20 +1,10 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
+from db import get_connection, init_db
 
 app = FastAPI()
 
-# allowing frontend to connect
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+init_db()
 
-@app.get("/api/hello")
-def hello():
-    return {"message": "backend is running!"}
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+@app.get("/")
+def home():
+    return {"message": "Backend connected to PostgreSQL"}
